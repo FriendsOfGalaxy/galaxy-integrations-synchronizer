70f39c8c5011830bc5d04d408d86f138a3f6e95c
FriendsOfGalaxy <friendsofgalaxy@gmail.com>
2019-08-30 15:31:25 +0200

    working poc

--- tasks.py
+++ tasks.py
@@ -9,8 +9,10 @@ import contextlib
 
 GITHUB = 'https://github.com'
 FOG = 'FriendsOfGalaxy'
+FOG_EMAIL = 'friendsofgalaxy@gmail.com'
 FILES_TO_EXCLUDE = ['README.md', '.travis.yml']
 FOG_BASE_BRANCH = 'master'
+PR_BRANCH = 'autoupdate'
 
 TEST_FORK_NAME = 'test-integration-fork'
 TEST_UPSTREAM = 'FriendsOfGalaxyTester/test-integration'
@@ -34,23 +36,45 @@ def get_version(abs_path):
         return json.load(f)['version']
 
 
+def init(c):
+    res = c.run(f"git clone {GITHUB}/{FOG}/{fork_name}")
+    with chdir(str(fork_dir)):
+        c.run(f'git config user.name {FOG}')
+        c.run(f'git config user.email {FOG_EMAIL}')
+
+        c.run(f'git remote add upstream {GITHUB}/{upstream}')
+        # try:
+        #     c.run(f'git remote add origin {GITHUB}/{FOG}/{fork_name}')
+        # except exceptions.UnexpectedExit:   # todo check default
+        c.run(f'git remote set-url origin {GITHUB}/{FOG}/{fork_name}')
+
+
+@task
+def create_branch(c, name=PR_BRANCH):
+    c.run(f'git checkout {PR_BRANCH} || git checkout -b {PR_BRANCH} && git pull origin {PR_BRANCH}', warn=True)
+    c.run(f'git branch {name}')
+    c.run(f'git push --set-upstream origin {name}')
+
+
 @task
 def sync(c, fork_name=TEST_FORK_NAME, upstream=TEST_UPSTREAM, release_branch=TEST_RELEASE_BRANCH, manifest_location=TEST_MANIFEST_LOCATION):
     """Requires env variable GITHUB_TOKEN (for hub) to not be prompt with password
     :param fork_name: name of current git name fork
     :upstream: github <user/repo_name> pattern
+    "release_branch: branch to be checked for new updates
+    :manifest_location: from root
     """
     root = pathlib.Path()
     fork_dir = root / fork_name
 
     if not fork_dir.exists():
-        res = c.run(f"git clone {GITHUB}/{FOG}/{fork_name}")
-        with chdir(str(fork_dir)):
-            c.run(f'git remote add upstream {GITHUB}/{upstream}')
+        c.run(f"inv init")
 
     with chdir(str(fork_dir)):
         c.run('git fetch upstream')
-        c.run(f'git checkout autoupdate || git checkout -b autoupdate && git pull origin autoupdate', warn=True)
+        c.run(f'git checkout {PR_BRANCH} || git checkout -b {PR_BRANCH} && git pull origin {PR_BRANCH}', warn=True)
+        c.run(f'git push --set-upstream origin {PR_BRANCH}')
+
         print(f'merging latest release branch {release_branch}')
         c.run(f'git merge --no-commit --no-ff upstream/{release_branch}')
 
@@ -67,8 +91,8 @@ def sync(c, fork_name=TEST_FORK_NAME, upstream=TEST_UPSTREAM, release_branch=TES
         pr_message = "Sync with the original repository"
         try:
             c.run(
-                f'hub pull-request --base {FOG}:{FOG_BASE_BRANCH} --head {FOG}:{FOG_BASE_BRANCH} --push '
-                f'-m {pr_title} -m {pr_message} --reviewer FriendsOfGalaxy --labels autoupdate --browse'
+                f'hub pull-request --push --base {FOG}:{FOG_BASE_BRANCH} --head {FOG}:{PR_BRANCH} '
+                f'-m "{pr_title}" -m "{pr_message}" --labels autoupdate --browse'
             )
         except Exception as e:
             print(e)
