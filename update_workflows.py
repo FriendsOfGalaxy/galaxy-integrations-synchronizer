83dc5897cc2c86020db5bd49e1edcf399d9c3e0d
FriendsOfGalaxy <FriendsOfGalaxy@gmail.com>
2019-10-18 15:41:36 +0200

    fix for sync workflows

--- update_workflows.py
+++ update_workflows.py
@@ -4,23 +4,38 @@ import os
 import json
 import subprocess
 import shutil
+import shlex
 import glob
 
 
 class GitUserContext:
-    def __init__(self, token, user):
+    def __init__(self, token, user, email):
         self.token = token
         self.user = user
+        self.email = email
+        self._run(f'git config --global user.email "{self.email}"')
+        self._run(f'git config --global user.name "{self.user}Bot"')
+
+    def _run(self, cmd: str, **kwargs):
+        kwargs.setdefault("stdout", subprocess.PIPE)
+        kwargs.setdefault("stderr", subprocess.STDOUT)
+        kwargs.setdefault("text", True)
+        cmd = shlex.split(cmd)
+        print('-- executing', cmd)
+        proc = subprocess.run(cmd, **kwargs)
+        print(proc.stdout)
+        proc.check_returncode()
 
     def clone_repo(self, repo):
-        subprocess.run(['git', 'clone', f'https://{self.user}:{self.token}@github.com/{self.user}/{repo}.git'])
+        self._run(f'git clone https://{self.user}:{self.token}@github.com/{self.user}/{repo}.git')
 
     def update_workflows(self, repo):
+        print('> copying workflow files')
         target = os.path.join(repo, '.github', 'workflows')
         for file_ in glob.glob(r'templates/.github/workflows/*.yml'):
             shutil.copy(file_, target)
-        subprocess.run(['git', 'commit', '-a', '-m', "Workflows autoupdate"], cwd=repo)
-        subprocess.run(['git', 'push', 'origin', 'master'], cwd=repo)
+        self._run(f'git commit -a -m "Workflows autoupdate"', cwd=repo)
+        self._run(f'git push origin master', cwd=repo)
 
 
 if __name__ == "__main__":
@@ -28,7 +43,7 @@ if __name__ == "__main__":
         names = json.load(f)['forks_to_sync']
 
     tkn = os.environ['GITHUB_TOKEN']
-    fog = GitUserContext(tkn, "FriendsOfGalaxy")
+    fog = GitUserContext(tkn, 'FriendsOfGalaxy', 'FriendsOfGalaxy@gmail.com')
     for repo_name in names:
         fog.clone_repo(repo_name)
         fog.update_workflows(repo_name)
